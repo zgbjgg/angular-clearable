73bb41f4d89f2f4e330e78d4491aeb489762d87d
Giacomo Antolini <giacomo.antolini@gmail.com>
2013-05-26 17:24:47 +0200

    Initial commit.

---
+++ js/ng-clearable.js
@@ -0,0 +1,42 @@
+angular.module('ngClearable', [])
+    .directive('ngClearable', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            compile: function(tElement) {
+                var clearClass = 'clear_button',
+                    divClass = clearClass + '_div';
+
+                if (!tElement.parent().hasClass(divClass)) {
+                    tElement.wrap('<div style="position: relative;" class="' + divClass + '">' + tElement.html() + '</div>');
+                    tElement.after('<a style="position: absolute; cursor: pointer;" class="' + clearClass + '">&times;</a>');
+
+                    var btn = tElement.next();
+
+                    btn.css('font-size', Math.round(tElement.prop('offsetHeight')*0.8) + 'px');
+                    btn.css('top', '2px');
+                    btn.css('left', Math.round(tElement.prop('offsetWidth') - btn.prop('offsetWidth')*1.3) + 'px');
+
+                    return function(scope, iElement, iAttrs) {
+                        if (iElement[0].tagName == 'DIV') {
+                            var text = angular.element(iElement.children()[0]);
+
+                            btn.bind('mousedown', function(e) {
+                                text.val('');
+                                text.triggerHandler('input');
+                                e.preventDefault();
+                            });
+
+                            scope.$watch(iAttrs.ngModel, function (v) {
+                                if (v && v.length > 0) {
+                                    btn.css('display', 'block');
+                                } else {
+                                    btn.css('display', 'none');
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    });
